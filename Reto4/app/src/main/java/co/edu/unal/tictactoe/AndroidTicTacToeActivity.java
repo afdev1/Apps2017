0c990c6eebb8caf497230b262d1ec186151c411f
afdev <afdev@unal.edu.co>
2017-09-03 15:15:45 -0500

    Reto4

---
+++ Reto4/app/src/main/java/co/edu/unal/tictactoe/AndroidTicTacToeActivity.java
@@ -0,0 +1,220 @@
+package co.edu.unal.tictactoe;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import edu.harding.tictactoe.TicTacToeGame;
+
+public class AndroidTicTacToeActivity extends AppCompatActivity {
+
+    // Buttons making up the board
+    private
+    Button mBoardButtons[];
+    // Various text displayed
+    private TextView mInfoTextView;
+
+    private TicTacToeGame mGame;
+
+    private int human = 0;
+    private int ties = 0;
+    private int android = 0;
+
+    private TextView mHumanTextView;
+    private TextView mTiesTextView;
+    private TextView mAndroidTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mBoardButtons = new Button[TicTacToeGame.BOARD_SIZE];
+        mBoardButtons[0] = (Button) findViewById(R.id.one);
+        mBoardButtons[1] = (Button) findViewById(R.id.two);
+        mBoardButtons[2] = (Button) findViewById(R.id.three);
+        mBoardButtons[3] = (Button) findViewById(R.id.four);
+        mBoardButtons[4] = (Button) findViewById(R.id.five);
+        mBoardButtons[5] = (Button) findViewById(R.id.six);
+        mBoardButtons[6] = (Button) findViewById(R.id.seven);
+        mBoardButtons[7] = (Button) findViewById(R.id.eight);
+        mBoardButtons[8] = (Button) findViewById(R.id.nine);
+        mInfoTextView = (TextView) findViewById(R.id.information);
+        mHumanTextView = (TextView) findViewById(R.id.human);
+        mTiesTextView = (TextView) findViewById(R.id.ties);
+        mAndroidTextView = (TextView) findViewById(R.id.android);
+        mGame = new TicTacToeGame();
+        startNewGame();
+    }
+
+    // Set up the game board.
+    private void startNewGame() {
+        mGame.clearBoard();
+        // Reset all buttons
+        for (int i = 0; i < mBoardButtons.length; i++) {
+            mBoardButtons[i].setText(String.valueOf(TicTacToeGame.OPEN_SPOT));
+            mBoardButtons[i].setEnabled(true);
+            mBoardButtons[i].setOnClickListener(new ButtonClickListener(i));
+        }
+        // Human goes first
+        mInfoTextView.setText(R.string.first_human);
+        update();
+    }
+
+    void update(){
+        mHumanTextView.setText(String.valueOf(human));
+        mTiesTextView.setText(String.valueOf(ties));
+        mAndroidTextView.setText(String.valueOf(android));
+    }
+
+    void disable(){
+        for(Button button : mBoardButtons){
+            button.setEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    static final int DIALOG_DIFFICULTY_ID = 0;
+    static final int DIALOG_ABOUT_ID = 1;
+    static final int DIALOG_QUIT_ID = 2;
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.new_game:
+                startNewGame();
+                return true;
+            case R.id.ai_difficulty:
+                showDialog(DIALOG_DIFFICULTY_ID);
+                return true;
+            case R.id.about:
+                showDialog(DIALOG_ABOUT_ID);
+                return true;
+            case R.id.quit:
+                showDialog(DIALOG_QUIT_ID);
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        switch(id) {
+            case DIALOG_DIFFICULTY_ID:
+                builder.setTitle(R.string.difficulty_choose);
+                final CharSequence[] levels = {
+                        getResources().getString(R.string.difficulty_easy),
+                        getResources().getString(R.string.difficulty_harder),
+                        getResources().getString(R.string.difficulty_expert)};
+                builder.setSingleChoiceItems(levels, 2,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int item) {
+                                dialog.dismiss();   // Close dialog
+                                // TODO: Set the diff level of mGame based on which item was selected.
+                                mGame.setDifficultyLevel(TicTacToeGame.DifficultyLevel.valueOf(levels[item].toString()));
+                                startNewGame();
+                                // Display the selected difficulty level
+                                Toast.makeText(getApplicationContext(), levels[item],
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                dialog = builder.create();
+                break;
+            case DIALOG_ABOUT_ID:
+//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                Context context = getApplicationContext();
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+                View layout = inflater.inflate(R.layout.about_dialog, null);
+                builder.setView(layout);
+                builder.setPositiveButton("OK", null);
+                dialog = builder.create();
+                break;
+            case DIALOG_QUIT_ID:
+                // Create the quit confirmation dialog
+                builder.setMessage(R.string.quit_question)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                AndroidTicTacToeActivity.this.finish();
+                            }
+                        })
+                        .setNegativeButton(R.string.no, null);
+                dialog = builder.create();
+                break;
+        }
+        return dialog;
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+        int location ;
+        ButtonClickListener(int location) {
+            this.location  = location;
+        }
+        public void onClick(View view) {
+            if (mBoardButtons[location].isEnabled()) {
+                setMove(TicTacToeGame.HUMAN_PLAYER, location);
+                // If no winner yet, let the computer make a move
+                int winner = mGame.checkForWinner();
+                if (winner == 0) {
+                    mInfoTextView.setText(R.string.turn_computer);
+                    int move = mGame.getComputerMove();
+                    setMove(TicTacToeGame.COMPUTER_PLAYER, move);
+                    winner = mGame.checkForWinner();
+                }
+                if(winner == 0)
+                    mInfoTextView.setText(R.string.turn_human);
+                else if (winner == 1) {
+                    mInfoTextView.setText(R.string.result_tie);
+                    ties++;
+                    update();
+                    disable();
+                }
+                else if(winner == 2) {
+                    mInfoTextView.setText(R.string.result_human_wins);
+                    human++;
+                    update();
+                    disable();
+                }
+                else {
+                    mInfoTextView.setText(R.string.result_computer_wins);
+                    android++;
+                    update();
+                    disable();
+                }
+            }
+        }
+
+        private void setMove(char player, int location) {
+            mGame.setMove(player, location);
+            mBoardButtons[location].setEnabled(false);
+            mBoardButtons[location].setText(String.valueOf(player));
+            if(player == TicTacToeGame.HUMAN_PLAYER)
+                mBoardButtons[location].setTextColor(Color.rgb(0, 200, 0));
+            else
+                mBoardButtons[location].setTextColor(Color.rgb(200, 0, 0));
+        }
+    }
+}
+
+
+
