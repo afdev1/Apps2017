d32e1c261aeb1524e8e9ea25aa4de6f11e6687ba
Andres Felipe De Orcajo <afdev@unal.edu.co>
2017-11-26 12:09:52 -0500

    User filter

--- Reto10/app/src/main/java/afdev/unal/edu/co/webservices/MainActivity.java
+++ Reto10/app/src/main/java/afdev/unal/edu/co/webservices/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -16,20 +21,48 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     List<WifiZone> datos = new ArrayList<>();
+    List<WifiZone> datos2 = new ArrayList<>();
+
+    private String[] arraySpinner;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.sp_departamento);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                datos2.clear();
+                TextView textView = (TextView) view;
+                String q = textView.getText().toString();
+                for(WifiZone zone : datos){
+                    if(zone.getMunicipio().equals(q)){
+                        datos2.add(zone);
+                    }
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         mRecyclerView = findViewById(R.id.my_recycler_view);
 
         mRecyclerView.setHasFixedSize(true);
@@ -37,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new WifiZoneAdapter(datos, this);
+        mAdapter = new WifiZoneAdapter(datos2, this);
         mRecyclerView.setAdapter(mAdapter);
 
         new ExecuteTask().execute();
@@ -69,16 +102,33 @@ public class MainActivity extends AppCompatActivity {
                 HttpResponse httpResponse = httpClient.execute(httpGet);
                 s = Consts.readResponse(httpResponse);
 
+                final HashSet<String> municipios = new HashSet<>();
                 JSONArray jsonArray = new JSONArray(s);
                 for (int i = 0; i < jsonArray.length(); i++) {
                     JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    datos.add(new WifiZone(getDato(jsonObject, "nombre_zona_wifi"), getDato(jsonObject,"municipio"), getDato(jsonObject,"latitud"),
-                            getDato(jsonObject,"longitud"), getDato(jsonObject,"departamento"), getDato(jsonObject,"direccion")));
+                    try {
+                        if (!getDato(jsonObject, "municipio").equals("")) {
+                            municipios.add(getDato(jsonObject, "municipio"));
+                        }
+                    } catch (Exception e) {
+
+                    }
+
+                    datos.add(new WifiZone(getDato(jsonObject, "nombre_zona_wifi"), getDato(jsonObject, "municipio"), getDato(jsonObject, "latitud"),
+                            getDato(jsonObject, "longitud"), getDato(jsonObject, "departamento"), getDato(jsonObject, "direccion")));
                 }
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         mAdapter.notifyDataSetChanged();
+
+                        arraySpinner = municipios.toArray(new String[municipios.size()]);
+                        Arrays.sort(arraySpinner);
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_item, arraySpinner);
+                        spinner.setAdapter(adapter);
+
+                        spinner.setSelection(38);
                     }
                 });
             } catch (Exception exception) {
@@ -88,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public String getDato(JSONObject obj, String param){
+    public String getDato(JSONObject obj, String param) {
         try {
             return obj.getString(param);
         } catch (JSONException e) {
